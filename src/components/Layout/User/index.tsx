32be80bfe18bc0b0ad7aa3d18d1bc5e2471c7c44
slogup-dale <dyddn304@gmail.com>
2022-06-29 16:51:03 +0900

    add: 히스토리 로직 추가

--- src/components/Layout/User/index.tsx
+++ src/components/Layout/User/index.tsx
@@ -1,25 +1,32 @@
-import React from "react";
-import {
-  UserContext,
-  ThemeContext,
-  SetStateContext,
-  StateContext,
-} from "../../../App";
+// import React from "react";
+// import {
+//   UserContext,
+//   ThemeContext,
+//   SetStateContext,
+//   StateContext,
+// } from "../../../App";
 
-const User = () => {
-  return (
-    <div>
-      유저
-      <UserContext.Consumer>{(value) => <p>{value}</p>}</UserContext.Consumer>
-      <ThemeContext.Consumer>{(value) => <p>{value}</p>}</ThemeContext.Consumer>
-      <StateContext.Consumer>
-        {(value) => <p>나이 : {value.age}</p>}
-      </StateContext.Consumer>
-      <StateContext.Consumer>
-        {(value) => <p>이름 : {value.name}</p>}
-      </StateContext.Consumer>
-    </div>
-  );
+// const User = () => {
+//   return (
+//     <div>
+//       유저
+//       <UserContext.Consumer>{(value) => <p>{value}</p>}</UserContext.Consumer>
+//       <ThemeContext.Consumer>{(value) => <p>{value}</p>}</ThemeContext.Consumer>
+//       <StateContext.Consumer>
+//         {(value) => <p>나이 : {value.age}</p>}
+//       </StateContext.Consumer>
+//       <StateContext.Consumer>
+//         {(value) => <p>이름 : {value.name}</p>}
+//       </StateContext.Consumer>
+//     </div>
+//   );
+// };
+
+// export default User;
+import React from "react";
+
+const index = () => {
+  return <div></div>;
 };
 
-export default User;
+export default index;
